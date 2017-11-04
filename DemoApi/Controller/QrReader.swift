8cc330cf90e0de4d738f8e5129672a4a526e914a
Amresh Subedi <amresh.subedi1@gmail.com>
2017-11-04 07:14:39 +0545

    some minor changes in staryboard

---
+++ DemoApi/Controller/QrReader.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  DemoApi
+//
+//  Created by owner on 10/29/17.
+//  Copyright © 2017 owner. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+class QrReader: ViewController {
+
+    @IBOutlet weak var square: UIImageView!
+    var video = AVCaptureVideoPreviewLayer()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        //Creating session
+        let session = AVCaptureSession()
+        
+        //Define capture devcie
+        let captureDevice = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        do
+        {
+            let input = try AVCaptureDeviceInput(device: captureDevice!)
+            session.addInput(input)
+        }
+        catch
+        {
+            print ("ERROR")
+        }
+        
+        let output = AVCaptureMetadataOutput()
+        session.addOutput(output)
+        
+        output.setMetadataObjectsDelegate(self as? AVCaptureMetadataOutputObjectsDelegate, queue: DispatchQueue.main)
+        
+        output.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+        
+        video = AVCaptureVideoPreviewLayer(session: session)
+        video.frame = view.layer.bounds
+        view.layer.addSublayer(video)
+        
+        self.view.bringSubview(toFront: square)
+        
+        
+        session.startRunning()
+    }
+    
+    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
+        
+        if metadataObjects != nil && metadataObjects.count != 0
+        {
+            if let object = metadataObjects[0] as? AVMetadataMachineReadableCodeObject
+            {
+                if object.type == AVMetadataObject.ObjectType.qr
+                {
+                    performSegue(withIdentifier: "Qrsegue", sender: nil)
+                }
+            }
+        }
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+
+}
